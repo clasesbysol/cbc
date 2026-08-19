6a27aa7f028dfd6f8d3f8ef847d714eb9e0215a5
clasesbysol <clasesbysol@gmail.com>
2026-08-19 00:32:30 -0300

    Refresh PWA cache for CBC v0.2.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cbc-x-solved-v0.2.1';
-const CORE=['./','./index.html','./styles-v2.css?v=2.1','./app-core.js?v=2.1','./app-data.js?v=2.1','./app-ui.js?v=2.1','./app-bind.js?v=2.1','./config.js','./manifest.webmanifest','./sun.svg'];
+const CACHE='cbc-x-solved-v0.2.2';
+const CORE=['./','./index.html','./styles-v2.css?v=2.2','./app-core.js?v=2.2','./app-data.js?v=2.2','./app-ui.js?v=2.2','./app-bind.js?v=2.2','./config.js?v=2.2','./manifest.webmanifest','./sun.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
